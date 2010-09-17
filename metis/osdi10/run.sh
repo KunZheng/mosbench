c3662fc586fe84a1d727b123f67bb688d2bea0ce
Yandong Mao <ydmao@mit.edu>
2010-09-17 13:57:36 -0400

    Add micro benchmarks

--- metis/osdi10/run.sh
+++ metis/osdi10/run.sh
@@ -3,15 +3,27 @@
 # login as root, and run tools/mount.sh to mount hugetlbfs with
 # sufficient memory.
 
+if [ "$#" -lt "1" ]; then
+  echo "Usage: $0 [min number of cores] [max numbeer of cores]"
+  exit;
+fi
 ncores=`grep -c processor /proc/cpuinfo`
 user=`id -u --name`
-
+if [ "$#" -eq "2" ]; then
+  max=$2
+else
+  max=$ncores
+fi
+echo cores: $1-$max
 sudo chown $user /mnt/huge -R
 
-for ((i=1; i<=ncores; i++))
+for ((i=$1; i<=max; i++))
 do
   rm /mnt/huge/* 2>/dev/null
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:obj/lib obj/app/wrmem.sf -p $i
+  #./tools/perf record -g obj/app/wrmem.sf -p $i
+  echo $i >> results/breakdown
+  sudo /usr/bin/time -f "user\t%U\tsys\t%S\treal\t%E" obj/app/wrmem.sf -p $i >> results/profile 2>>results/breakdown
+   #sudo obj/app/wrmem.sf -p $i
 done
 
 # login as root, and run tools/umount.sh to umount hugetlbfs
