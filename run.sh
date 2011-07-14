a84becbe2672de3ab3aaf1a8e5643cfcbc54b0c7
Silas Boyd-Wickizer <sbw@mit.edu>
2011-07-14 01:16:38 -0400

    A script for running a benchmark from the command line

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ROOT=/home/sbw/mosbench
+
+make_config()
+{
+    (cd $ROOT;
+	echo "#" > config.py;
+	echo "# XXX DO NOT EDIT (generated from config.tmpl)" >> config.py;
+	echo "#" >> config.py;
+	cat config.tmpl | sed "s/CONFIG_SPACE/$1.merge(shared)/" >> config.py;
+    )
+}
+
+case "$1" in
+    exim)
+	make_config "exim"
+	(cd $ROOT && ./mkmounts tmpfs-separate 0 0)
+	(cd $ROOT && make bench)
+	;;
+
+    metis)
+	make_config "metis"
+	(cd $ROOT && ./mkmounts hugetlb 0 0)
+	(cd $ROOT && make bench)
+	;;
+    *)
+	echo "ERROR: unknown $1"
+	exit 2
+	;;
+esac
