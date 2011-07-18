503463655dc0c1dc95f95af15a6b03a5c1f5bcb0
Silas Boyd-Wickizer <sbw@mit.edu>
2011-07-18 03:17:02 -0400

    run.sh -- tweaks and postgres support

--- run.sh
+++ run.sh
@@ -15,25 +15,31 @@ make_config()
 case "$1" in
     exim)
 	make_config "exim"
-	(cd $ROOT && ./mkmounts tmpfs-separate 0 0)
+	(cd $ROOT && sudo ./mkmounts tmpfs-separate)
 	(cd $ROOT && make bench)
 	;;
 
     metis)
 	make_config "metis"
-	(cd $ROOT && ./mkmounts hugetlb 0 0)
+	(cd $ROOT && sudo ./mkmounts hugetlb)
 	(cd $ROOT && make bench)
 	;;
 
     gmake)
 	make_config "gmake"
-	(cd $ROOT && ./mkmounts tmpfs-separate 0 0)
+	(cd $ROOT && sudo ./mkmounts tmpfs-separate)
 	(cd $ROOT && make bench)
 	;;
 
     psearchy)
 	make_config "psearchy"
-	(cd $ROOT && ./mkmounts tmpfs-separate 0 0)
+	(cd $ROOT && sudo ./mkmounts tmpfs-separate)
+	(cd $ROOT && make bench)
+	;;
+
+    postgres)
+	make_config "postgres"
+	(cd $ROOT && sudo ./mkmounts tmpfs-separate)
 	(cd $ROOT && make bench)
 	;;
 
